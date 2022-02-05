a54dcd0a5ece9135467e08d122be4fe1f15d4313
Rohan <rrjagtap@uwaterloo.ca>
2022-02-05 02:15:40 -0500

    Started React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9au9Y-Bhx4C7Uf3xA-b3G9X0-9RoMNhU",
+  authDomain: "testapp-dd266.firebaseapp.com",
+  databaseURL: "https://testapp-dd266.firebaseio.com",
+  projectId: "testapp-dd266",
+  storageBucket: "testapp-dd266.appspot.com",
+  messagingSenderId: "97391103528",
+  appId: "1:97391103528:web:45bbba0e98743fda79af1d",
+  measurementId: "G-NXEC03F6GB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
